61dcf54fdfca928b771f06bd917d97bf34cb71a1
phothet <phothet@gmail.com>
2018-03-10 18:53:00 -0500

    update code format

--- mkix2fw2usb.sh
+++ mkix2fw2usb.sh
@@ -12,27 +12,27 @@ tar xzvf $ofw -C $ix2/update/
 
 imgs=$(find $ix2/update/)
 for img in ${imgs} ; do
-	  if [ -f $img.md5 ] ; then
-		      mv $img $ix2/images/
-		          mv $img.md5 $ix2/images/
-			    fi
-		    done
+	if [ -f $img.md5 ] ; then
+	  mv $img $ix2/images/
+		mv $img.md5 $ix2/images/
+	fi
+done
 
-		    mount -o loop,ro $ix2/images/apps $ix2/apps
-		    cp -p $ix2/apps/usr/local/cfg/config.gz $ix2/images/
-		    umount $ix2/apps
-		    gunzip $ix2/images/config.gz
+mount -o loop,ro $ix2/images/apps $ix2/apps
+cp -p $ix2/apps/usr/local/cfg/config.gz $ix2/images/
+umount $ix2/apps
+gunzip $ix2/images/config.gz
 
-		    img=$ix2/images/config
-		    md5=$(md5sum $img)
-		    md5=${md5% *}
-		    md5=${md5% }
-		    echo "$md5" > $img.md5
+img=$ix2/images/config
+md5=$(md5sum $img)
+md5=${md5% *}
+md5=${md5% }
+echo "$md5" > $img.md5
 
-		    cd $ix2/images/
-		    tar czvf ../ix2-boot.tgz *
+cd $ix2/images/
+tar czvf ../ix2-boot.tgz *
 			
-		    # ix2-200d_images doesn't work
-		    # https://www.technopat.net/sosyal/konu/upgrading-iomega-ix2-200-to-cloud-edition.2651/
-		    mkdir -p $usb/emctools/ix2-200_images/
-		    cp $ix2/ix2-boot.tgz $usb/emctools/ix2-200_images/
+# ix2-200d_images doesn't work
+# https://www.technopat.net/sosyal/konu/upgrading-iomega-ix2-200-to-cloud-edition.2651/
+mkdir -p $usb/emctools/ix2-200_images/
+cp $ix2/ix2-boot.tgz $usb/emctools/ix2-200_images/
