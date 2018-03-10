11eb9d3617193acdb447d112009af6929d769bfa
phothet <phothet@gmail.com>
2018-03-10 18:49:41 -0500

    remove comments

--- mkix2fw2usb.sh
+++ mkix2fw2usb.sh
@@ -1,10 +1,3 @@
-# usb needs to be formatted as fat32 type c
-# after formatting the stick, don't forget to run mkfs.vfat
-# all the files needs to be owned by root user and group
-# run the script as root user
-# using ix2-200d_images directory doesn't work
-# https://www.technopat.net/sosyal/konu/upgrading-iomega-ix2-200-to-cloud-edition.2651/
-# http://iomega.nas-central.org/wiki/Category:Ix2-200-usb-init
 ifw=/home/phothet/Downloads/ix2-200-3.2.14.30167.tgz
 ofw=${ifw%.tgz}-decrypted.tar.gz
 ix2=${ifw%.tgz}-files
